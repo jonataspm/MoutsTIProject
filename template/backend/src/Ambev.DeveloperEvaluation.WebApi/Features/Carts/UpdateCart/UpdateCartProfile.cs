610e07b1640d879544417ad7a466902f727be35d
jonataspm <jonataspmpm@gmail.com>
2026-04-26 12:42:35 -0300

    feat: Criando UpdateCartProfile

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Carts/UpdateCart/UpdateCartProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Carts.UpdateCart;
+
+public class UpdateCartProfile : Profile
+{
+    public UpdateCartProfile()
+    {
+        CreateMap<UpdateCartRequest, Application.Carts.UpdateCart.UpdateCartCommand>();
+        CreateMap<Application.Carts.UpdateCart.UpdateCartResult, UpdateCartResponse>();
+    }
+}
